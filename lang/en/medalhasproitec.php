ffa2a9b6071f33c2947969689e9622a34b554524
Kelson da Costa Medeiros <kelsoncm@gmail.com>
2025-06-11 15:32:12 -0300

    feat: [add] initial version

---
+++ lang/en/medalhasproitec.php
@@ -0,0 +1,48 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * Plugin strings are defined here.
+ *
+ * @package     mod_medalhasproitec
+ * @category    string
+ * @copyright   2025 DEAD/ZL/IFRN <dead.zl@ifrn.edu.br>, Kelson da Costa Medeiros <kelsoncm@gmail.com>
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['messageprovider:expiry'] = 'Expiry message';
+$string['messageprovider:submission'] = 'Submission message';
+$string['missingidandcmid'] = 'Missing id and cmid';
+$string['modulename'] = 'Medalhas Proitec';
+$string['modulename_help'] = 'Use the newmodule module for...';
+$string['modulenameplural'] = 'Medalhas Proitec';
+$string['medalhasproitec:addinstance'] = 'Add instance';
+$string['medalhasproitecfieldset'] = 'Medalhas Proitec fieldset';
+$string['medalhasproitecname'] = 'Medalhas Proitec';
+$string['medalhasproitecname_help'] = 'Medalhas Proitec';
+$string['medalhasproitecsettings'] = 'Medalhas Proitec settings';
+$string['newmodulefieldset'] = 'Custom example fieldset.';
+$string['newmodulename'] = 'newmodule name';
+$string['newmodulename_help'] = 'This is the content of the help tooltip associated with the newmodulename field';
+$string['newmodulesettings'] = 'Settings';
+$string['nomedalhasproitecinstances'] = 'Medalhas Proitec instances';
+$string['nonewmodules'] = 'No newmodules';
+$string['pluginadministration'] = 'newmodule administration';
+$string['pluginname'] = 'Medalhas Proitec';
+$string['privacy:metadata'] = 'Medalhas Proitec does not store any personal data';
+$string['view'] = 'View';
