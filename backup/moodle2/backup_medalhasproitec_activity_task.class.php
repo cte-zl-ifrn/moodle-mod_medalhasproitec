f1340446b40686f79f356d759911de8d556b8373
Kelson da Costa Medeiros <kelsoncm@gmail.com>
2025-06-21 21:59:51 -0300

    fix: não necessita corrigir os links

--- backup/moodle2/backup_medalhasproitec_activity_task.class.php
+++ backup/moodle2/backup_medalhasproitec_activity_task.class.php
@@ -28,25 +28,26 @@ defined('MOODLE_INTERNAL') || die();
 // More information about the backup process: {@link https://docs.moodle.org/dev/Backup_API}.
 // More information about the restore process: {@link https://docs.moodle.org/dev/Restore_API}.
 
-require_once($CFG->dirroot.'/mod/medalhasproitec/backup/moodle2/backup_medalhasproitec_stepslib.php');
-require_once($CFG->dirroot.'/mod/medalhasproitec/backup/moodle2/backup_medalhasproitec_settingslib.php');
+require_once($CFG->dirroot . '/mod/medalhasproitec/backup/moodle2/backup_medalhasproitec_stepslib.php');
+require_once($CFG->dirroot . '/mod/medalhasproitec/backup/moodle2/backup_medalhasproitec_settingslib.php');
 
 /**
  * Provides all the settings and steps to perform a complete backup of mod_medalhasproitec.
  */
-class backup_medalhasproitec_activity_task extends backup_activity_task {
+
+class backup_medalhasproitec_activity_task extends backup_activity_task
+{
 
     /**
      * Defines particular settings for the plugin.
      */
-    protected function define_my_settings() {
-        return;
-    }
+    protected function define_my_settings() {}
 
     /**
      * Defines particular steps for the backup process.
      */
-    protected function define_my_steps() {
+    protected function define_my_steps()
+    {
         $this->add_step(new backup_medalhasproitec_activity_structure_step('medalhasproitec_structure', 'medalhasproitec.xml'));
     }
 
@@ -56,19 +57,8 @@ class backup_medalhasproitec_activity_task extends backup_activity_task {
      * @param string $content
      * @return string
      */
-    public static function encode_content_links($content) {
-        global $CFG;
-
-        $base = preg_quote($CFG->wwwroot, '/');
-
-        // Link to the list of choices.
-        $search = "/(".$base."\//mod\/medalhasproitec\/index.php\?id\=)([0-9]+)/";
-        $content = preg_replace($search, '$@medalhasproitecINDEX*$2@$', $content);
-
-        // Link to choice view by moduleid.
-        $search = "/(".$base."\//mod\/medalhasproitec\/view.php\?id\=)([0-9]+)/";
-        $content = preg_replace($search, '$@medalhasproitecVIEWBYID*$2@$', $content);
-
+    public static function encode_content_links($content)
+    {
         return $content;
     }
 }
